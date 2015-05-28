930b2eab2187a512b2e9d127ca9b64955f4e8ceb
dvanmelzen <daniel.van.melzen@student.stenden.com>
2015-05-28 11:48:16 +0200

    Hallo

--- TheBeerGame/Program.cs
+++ TheBeerGame/Program.cs
@@ -12,7 +12,7 @@ namespace TheBeerGame
         /// </summary>
         static void Main()
         {
-            var factory = new MonoGame.Framework.GameFrameworkViewSource<Game1>();
+            var factory = new MonoGame.Framework.GameFrameworkViewSource<MainMenu>();
             Windows.ApplicationModel.Core.CoreApplication.Run(factory);
         }
     }
